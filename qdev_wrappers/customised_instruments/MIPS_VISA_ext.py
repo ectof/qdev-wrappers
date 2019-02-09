50ff02265ba9fcd7c9120e7213b1432d8d8ced65
Qdev Triton11 <qdevtriton11@gmail.com>
2019-02-09 15:58:25 +0100

    blocking simultaneous ramp and additional features on william implementation of blockin safe ramps

--- qdev_wrappers/customised_instruments/MIPS_VISA_ext.py
+++ qdev_wrappers/customised_instruments/MIPS_VISA_ext.py
@@ -1,32 +1,87 @@
 from qcodes.instrument_drivers.oxford.MercuryiPS_VISA import MercuryiPS
+from typing import Dict, Union, Optional, Callable, List, cast
+import numpy as np
+import time
 
 
 class MercuryiPS_ext(MercuryiPS):
 
 	def __init__(self, name, address,
-                 field_limits=None,
-                 **kwargs) -> None:
+				 field_limits=None,
+				 **kwargs) -> None:
 
 		super().__init__(name, address,
-			             field_limits=field_limits, **kwargs)
+						 field_limits=field_limits, **kwargs)
 		
 		self.add_parameter('theta_saferamp',
-		                   label='Theta',
+						   label='Theta',
 						   unit='degrees',
 						   set_cmd=self._theta_saferamp_set,
 						   get_cmd=self._theta_saferamp_get)
 		
 		self.add_parameter('r_saferamp',
-		                   label='B_Radius',
-						   unit='Tesla',
+						   label='B_Radius',
+						   unit='T',
 						   set_cmd=self._r_saferamp_set,
 						   get_cmd=self._r_saferamp_get)
 
+		self.add_parameter('r_simulramp',
+						   label='B_Radius',
+						   unit='T',
+						   set_cmd=self._r_simulramp_set,
+						   get_cmd=self._r_simulramp_get)
+
 		self.add_parameter('phi_saferamp',
-		                   label='B_Radius',
-						   unit='Tesla',
+						   label='Phi',
+						   unit='degrees',
 						   set_cmd=self._phi_saferamp_set,
 						   get_cmd=self._phi_saferamp_get)
+
+	def ramp(self, mode: str="safe") -> None:
+		"""
+		Ramp the fields to their present target value
+
+		Args:
+			mode: how to ramp, either 'simul' or 'safe'. In 'simul' mode,
+			  the fields are ramping simultaneously in a non-blocking mode.
+			  There is no safety check that the safe zone is not exceeded. In
+			  'safe' mode, the fields are ramped one-by-one in a blocking way
+			  that ensures that the total field stays within the safe region
+			  (provided that this region is convex).
+		"""
+		if mode not in ['simul', 'safe', 'simul_block']:
+			raise ValueError('Invalid ramp mode. Please provide either "simul"'
+							 ',"safe" or "simul_block".')
+
+		meas_vals = self._get_measured(['x', 'y', 'z'])
+		# we asked for three coordinates, so we know that we got a list
+		meas_vals = cast(List[float], meas_vals)
+
+		for cur, slave in zip(meas_vals, self.submodules.values()):
+			if slave.field_target() != cur:
+				if slave.field_ramp_rate() == 0:
+					raise ValueError(f'Can not ramp {slave}; ramp rate set to'
+									 ' zero!')
+
+		# then the actual ramp
+		{'simul': self._ramp_simultaneously,
+		 'safe': self._ramp_safely,
+		 'simul_block':self._ramp_simultaneously_blocking}[mode]()
+
+
+	def _ramp_simultaneously_blocking(self) -> None:
+		"""
+		Ramp all three fields to their target simultaneously at their given
+		ramp rates. NOTE: there is NO guarantee that this does not take you
+		out of your safe region. Use with care.
+		"""
+		for slave in self.submodules.values():
+			slave.ramp_to_target()
+
+		for slave in np.array(list(self.submodules.values())):
+			# wait for the ramp to finish, we don't car about the order
+			while slave.ramp_status() == 'TO SET':
+				time.sleep(0.1)
 
 	def _theta_saferamp_set(self, target):
 		"""
@@ -34,7 +89,7 @@ class MercuryiPS_ext(MercuryiPS):
 		"""
 		self.theta_target(target)
 		self.ramp(mode='safe')
-		self.theta_measured.get()
+		self.theta_measured()
 
 	def _theta_saferamp_get(self):
 		"""
@@ -48,9 +103,23 @@ class MercuryiPS_ext(MercuryiPS):
 		"""
 		self.r_target(target)
 		self.ramp(mode='safe')
-		self.r_measured.get()
+		self.r_measured()
 
 	def _r_saferamp_get(self):
+		"""
+		Get the measured radius
+		"""
+		return self.r_measured()
+
+	def _r_simulramp_set(self, target):
+		"""
+		Set the radius target and ramp to it
+		"""
+		self.r_target(target)
+		self.ramp(mode='simul_block')
+		self.r_measured()
+
+	def _r_simulramp_get(self):
 		"""
 		Get the measured radius
 		"""
@@ -62,7 +131,7 @@ class MercuryiPS_ext(MercuryiPS):
 		"""
 		self.phi_target(target)
 		self.ramp(mode='safe')
-		self.phi_measured.get()
+		self.phi_measured()
 
 	def _phi_saferamp_get(self):
 		"""
